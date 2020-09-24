427d6f2e0a40cff2d1a690276ca1b597c83affff
linux_china <libing.chen@gmail.com>
2020-09-23 21:38:36 -0700

    Button.svelte

---
+++ src/Button.svelte
@@ -0,0 +1,39 @@
+<svelte:options tag="cool-button"/>
+
+<script>
+    export let text = "Click me!!!";
+
+    let count = 0;
+
+    function clickHandler() {
+        count += 1;
+        text = "count: " + count;
+    }
+</script>
+
+
+<button on:click={clickHandler}>
+    {text}
+</button>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
